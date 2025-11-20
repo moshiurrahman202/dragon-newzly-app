769d746ee3018fa637081e5bc2af3bed641d4511
Mosiur Rahman sumon <mosiurrahman123654@gmail.com>
2025-11-20 14:37:25 +0600

    theme customiazation fint and date-fns integration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import logo from "../assets/logo.png"
+import { format } from "date-fns";
 
 const Header = () => {
     return (
         <div className="flex flex-col items-center gap-3">
             <img className="w-[471px]" src={logo} alt="this is logo" />
-            <p className="text-gray-500">Journalism Without Fear or Favour</p>
+            <p className="text-accent">Journalism Without Fear or Favour</p>
+            <p>{format(new Date(), "EEEE, MMMM dd, yyyy")}</p>
         </div>
     );
 };
