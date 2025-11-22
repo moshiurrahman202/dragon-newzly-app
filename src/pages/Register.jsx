925217badbafa3422f66d6c0f77b083aabbe34d9
Mosiur Rahman sumon <mosiurrahman123654@gmail.com>
2025-11-22 20:39:11 +0600

    register user firebase auth state change

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,20 +1,43 @@
+import { use } from "react";
 import { Link } from "react-router";
+import { AuthContext } from "../provider/AuthProvider";
+
 
 const Register = () => {
+    const {createUser} = use(AuthContext)
+    const formHandler = e => {
+        e.preventDefault()
+        const name = e.target.name.value
+        const email = e.target.email.value
+        const pass = e.target.password.value
+            createUser(email, pass)
+            .then(res => {
+                console.log("user created done");
+                console.log(res);
+                
+                
+                
+            })
+            .catch(err => {
+                console.log(err);
+                
+            })
+        
+    }
     return (
         <div className="flex justify-center">
            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5">
       <div className="card-body">
         <p className="font-semibold text-2xl text-center">Register Your Account</p>
-        <fieldset className="fieldset">
+        <form onSubmit={formHandler} className="fieldset">
           <label className="label">Your Name</label>
-          <input type="text" className="input" placeholder="Enter Your Name" name="name" />
+          <input required type="text" className="input" placeholder="Enter Your Name" name="name" />
           <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" name="email" />
+          <input required type="email" className="input" placeholder="Email" name="email" />
           <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" name="password" />
-          <button className="btn btn-neutral mt-4">Login</button>
-        </fieldset>
+          <input required type="password" className="input" placeholder="Password" name="password" />
+          <button type="submit" className="btn btn-neutral mt-4">Login</button>
+        </form>
         <p className="py-5">Alrady Have an Account ? <Link className="text-secondary" to="/auth/login">Login</Link></p>
       </div>
     </div>
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,11 +1,15 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { createContext } from "react";
+import auth from "../config/fireBaseConfig";
 
 
 export const AuthContext = createContext(null)
 const AuthProvider = ({children}) => {
+    const createUser = (email, pass) =>  {
+        return createUserWithEmailAndPassword(auth, email, pass)
+    }
     const userInfo = {
-        name: "sumon",
-        age: 23
+        createUser
     }
     return <AuthContext value={userInfo}>{children}</AuthContext>;
 };
